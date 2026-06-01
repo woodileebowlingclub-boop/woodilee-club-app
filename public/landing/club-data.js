6c7c9823d474c492d1eeae7313fbef97233be102
woodileebowlingclub-boop <woodileebowlingclub@gmail.com>
2026-06-01 17:30:36 +0100

    Add landing page club data

---
+++ public/landing/club-data.js
@@ -0,0 +1,167 @@
+window.WOODILEE_CLUB_DATA = {
+  "fixtures": [
+    {
+      "title": "Woodilee v St Rollox",
+      "opponent": "St Rollox",
+      "date": "2026-06-02",
+      "time": "19:30",
+      "team": "Woodilee"
+    },
+    {
+      "title": "Gents Seniors",
+      "opponent": "Gents Seniors",
+      "date": "2026-06-03",
+      "time": "14:00",
+      "team": "Woodilee"
+    },
+    {
+      "title": "Thursday Bounce",
+      "opponent": "Thursday Bounce",
+      "date": "2026-06-04",
+      "time": "18:45",
+      "team": "Woodilee"
+    }
+  ],
+  "mondayNightPoints": [
+    {
+      "name": "Kenny Cook",
+      "total": 14,
+      "played": 5
+    },
+    {
+      "name": "Aileen Miller",
+      "total": 14,
+      "played": 5
+    },
+    {
+      "name": "Ian Whiteford",
+      "total": 14,
+      "played": 6
+    },
+    {
+      "name": "Willie Gregory",
+      "total": 13,
+      "played": 5
+    },
+    {
+      "name": "Andy Sharp",
+      "total": 12,
+      "played": 5
+    },
+    {
+      "name": "Willie McIntyre",
+      "total": 11,
+      "played": 5
+    },
+    {
+      "name": "David Mitchell",
+      "total": 11,
+      "played": 6
+    },
+    {
+      "name": "Alex Maxwell",
+      "total": 11,
+      "played": 4
+    },
+    {
+      "name": "Davie Munro",
+      "total": 10,
+      "played": 3
+    },
+    {
+      "name": "Fiona Green",
+      "total": 8,
+      "played": 5
+    },
+    {
+      "name": "Ricky Irvine",
+      "total": 8,
+      "played": 6
+    },
+    {
+      "name": "Rab McLaughlin",
+      "total": 7,
+      "played": 3
+    },
+    {
+      "name": "Peter Barber",
+      "total": 7,
+      "played": 3
+    },
+    {
+      "name": "Ronnie McKinnon",
+      "total": 7,
+      "played": 5
+    },
+    {
+      "name": "Adam Turner",
+      "total": 7,
+      "played": 3
+    },
+    {
+      "name": "Alan Ralston",
+      "total": 6,
+      "played": 3
+    },
+    {
+      "name": "Charlie Cameron",
+      "total": 6,
+      "played": 2
+    },
+    {
+      "name": "Frank Devlin",
+      "total": 6,
+      "played": 3
+    },
+    {
+      "name": "Ross Gregory",
+      "total": 5,
+      "played": 2
+    },
+    {
+      "name": "Alan Gill",
+      "total": 5,
+      "played": 3
+    },
+    {
+      "name": "Willie Brown",
+      "total": 4,
+      "played": 1
+    },
+    {
+      "name": "Trevor Barraclough",
+      "total": 3,
+      "played": 1
+    },
+    {
+      "name": "Rita Gordon",
+      "total": 3,
+      "played": 3
+    },
+    {
+      "name": "Chris Moran",
+      "total": 3,
+      "played": 1
+    },
+    {
+      "name": "Chuck Irvine",
+      "total": 3,
+      "played": 1
+    },
+    {
+      "name": "Anne Carr",
+      "total": 2,
+      "played": 2
+    },
+    {
+      "name": "Jin McDonald",
+      "total": 1,
+      "played": 1
+    },
+    {
+      "name": "Karrie McDonald",
+      "total": 1,
+      "played": 1
+    }
+  ]
+};
